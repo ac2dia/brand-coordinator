9c93fef75ca21b836759c1977934f39b69b3d933
ac2dia <ac2dia@gmail.com>
2024-09-23 02:37:16 +0900

    fix: add transactional annotation

--- src/main/kotlin/com/example/brandcoordinator/domain/product/ProductServiceImpl.kt
+++ src/main/kotlin/com/example/brandcoordinator/domain/product/ProductServiceImpl.kt
@@ -11,17 +11,20 @@ import com.example.brandcoordinator.domain.product.dto.ProductResponse
 import com.example.brandcoordinator.domain.product.model.Product
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ProductServiceImpl(
     private val productRepository: ProductRepository,
     private val brandRepository: BrandRepository,
 ) : ProductService {
+    @Transactional(readOnly = true)
     override fun findAll(): List<ProductResponse> {
         val products = this.productRepository.findAll()
         return products.map { ProductResponse.from(product = it) }
     }
 
+    @Transactional
     override fun save(productPostRequest: ProductPostRequest) {
         val brand = this.findBrandByName(name = productPostRequest.brandName)
 
@@ -33,6 +36,7 @@ class ProductServiceImpl(
         this.productRepository.save(product)
     }
 
+    @Transactional
     override fun update(
         id: Long,
         productPatchRequest: ProductPatchRequest
@@ -52,11 +56,13 @@ class ProductServiceImpl(
         return ProductResponse.from(product = product)
     }
 
+    @Transactional
     override fun delete(id: Long) {
         val product = findById(id = id)
         this.productRepository.delete(product)
     }
 
+    @Transactional(readOnly = true)
     override fun findMaxAndMinProductsByCategory(category: String): CategoryBrandProductSummaryResponse {
         val products = this.productRepository.findByCategory(category = category)
             .ifEmpty { throw IllegalArgumentException("") }
@@ -68,11 +74,12 @@ class ProductServiceImpl(
         val minPriceProducts = products.filter { it.price == minPrice }
 
         return CategoryBrandProductSummaryResponse.from(
-            minimumPriceProducts = maxPriceProducts,
-            maximumPriceProducts = minPriceProducts,
+            minimumPriceProducts = minPriceProducts,
+            maximumPriceProducts = maxPriceProducts,
         )
     }
 
+    @Transactional(readOnly = true)
     override fun findLowestPriceProductsEachCategories(): BrandProductSummaryResponse {
         val products = this.productRepository.findProductsWithLowestPrice()
         val filteredProducts = products
@@ -84,6 +91,7 @@ class ProductServiceImpl(
         return BrandProductSummaryResponse.from(products = filteredProducts)
     }
 
+    @Transactional(readOnly = true)
     override fun findLowestPriceProductsWithBrand(): LowestBrandProductSummaryResponse {
         val products = this.productRepository.findProductsByBrandWithLowestTotalPrice()
         return LowestBrandProductSummaryResponse.from(products = products)
