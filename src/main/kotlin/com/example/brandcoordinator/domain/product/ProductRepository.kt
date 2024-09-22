e416f9dc200f4429e0ee89dbf06256cfa3c4fb32
ac2dia <ac2dia@gmail.com>
2024-09-22 18:20:21 +0900

    feat: implement product repository about summary by category

--- src/main/kotlin/com/example/brandcoordinator/domain/product/ProductRepository.kt
+++ src/main/kotlin/com/example/brandcoordinator/domain/product/ProductRepository.kt
@@ -2,8 +2,14 @@ package com.example.brandcoordinator.domain.product
 
 import com.example.brandcoordinator.domain.product.model.Product
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ProductRepository : JpaRepository<Product, Long> {
+    fun findByBrandId(brandId: Long): List<Product>
+
+    @Query("SELECT p FROM Product p JOIN FETCH p.brand WHERE p.category = :category")
+    fun findByCategory(@Param("category") category: String): List<Product>
 }
