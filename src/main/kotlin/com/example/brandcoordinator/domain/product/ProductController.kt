26e8740fa0235389711f399d6d2d00990e8ccb06
ac2dia <ac2dia@gmail.com>
2024-09-22 21:51:52 +0900

    feat: implement lowest brand product controller

--- src/main/kotlin/com/example/brandcoordinator/domain/product/ProductController.kt
+++ src/main/kotlin/com/example/brandcoordinator/domain/product/ProductController.kt
@@ -2,6 +2,7 @@ package com.example.brandcoordinator.domain.product
 
 import com.example.brandcoordinator.domain.product.dto.BrandProductSummaryResponse
 import com.example.brandcoordinator.domain.product.dto.CategoryBrandProductSummaryResponse
+import com.example.brandcoordinator.domain.product.dto.LowestBrandProductSummaryResponse
 import com.example.brandcoordinator.domain.product.dto.ProductPatchRequest
 import com.example.brandcoordinator.domain.product.dto.ProductPostRequest
 import com.example.brandcoordinator.domain.product.dto.ProductResponse
@@ -64,5 +65,12 @@ class ProductController(
         val brandProductSummaryResponse =
             this.productService.findLowestPriceProductsEachCategories()
         return ResponseEntity.ok(brandProductSummaryResponse)
+    }
+
+    @GetMapping("/api/v1/get-summary-lowest-brand")
+    fun getSummaryLowestBrand(): ResponseEntity<LowestBrandProductSummaryResponse> {
+        val lowestBrandProductSummaryResponse =
+            this.productService.findLowestPriceProductsWithBrand()
+        return ResponseEntity.ok(lowestBrandProductSummaryResponse)
     }
 }
